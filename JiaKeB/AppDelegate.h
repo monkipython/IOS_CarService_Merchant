c2e19863e49d732945c0733ff2b9f7408208d53b
DreamHgs <390809438@qq.com>
2014-09-13 09:43:11 +0800

    Initial Commit

---
+++ JiaKeB/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  JiaKeB
+//
+//  Created by hgs泽泓 on 14-9-13.
+//  Copyright (c) 2014年 HgsZehong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
